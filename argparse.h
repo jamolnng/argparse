2434484ed2c0bcee59bb8518f04272e056d04786
Jesse Laning <jlaning@ksu.edu>
2018-11-18 22:32:36 -0600

    prefixed private functions

--- argparse.h
+++ argparse.h
@@ -52,12 +52,12 @@ class ArgumentParser {
                           [](std::string a, std::string b) {
                             return a + " " + std::string(b);
                           });
-      std::vector<std::string> split_args = split(argstr, "^-|\\s-");
+      std::vector<std::string> split_args = _split(argstr, "^-|\\s-");
       std::vector<std::string> arg_parts;
       std::string name;
       for (auto &split_arg : split_args) {
-        std::vector<std::string> arg_parts = split(split_arg, "\\s+");
-        name = trim_copy(arg_parts[0]);
+        std::vector<std::string> arg_parts = _split(split_arg, "\\s+");
+        name = _trim_copy(arg_parts[0]);
         if (name.empty()) continue;
         if (name[0] == '-') {
           _add_variable(name, arg_parts, argv);
@@ -80,7 +80,7 @@ class ArgumentParser {
   }
 
   bool exists(const std::string &name) {
-    return _variables.find(delimit(name)) != _variables.end();
+    return _variables.find(_delimit(name)) != _variables.end();
   }
 
   template <typename T>
@@ -120,69 +120,69 @@ class ArgumentParser {
       _help = true;
       print_help(argv[0]);
     }
-    ltrim(name, [](int c) { return c != (int)'-'; });
-    name = delimit(name);
+    _ltrim(name, [](int c) { return c != (int)'-'; });
+    name = _delimit(name);
     std::string val;
     if (arg_parts.size() > 1)
       val = std::accumulate(arg_parts.begin() + 1, arg_parts.end(),
                             std::string(), [](std::string a, std::string b) {
-                              return trim_copy(a) + " " + trim_copy(b);
+                              return _trim_copy(a) + " " + _trim_copy(b);
                             });
     else
       val = "";
     _variables[name] = val;
   }
-  static std::string delimit(const std::string &name) {
+  static std::string _delimit(const std::string &name) {
     return std::string(std::min(name.size(), (size_t)2), '-').append(name);
   }
-  static std::string strip(const std::string &name) {
+  static std::string _strip(const std::string &name) {
     size_t begin = 0;
     begin += name.size() > 0 ? name[0] == '-' : 0;
     begin += name.size() > 3 ? name[1] == '-' : 0;
     return name.substr(begin);
   }
-  static std::string upper(const std::string &in) {
+  static std::string _upper(const std::string &in) {
     std::string out(in);
     std::transform(out.begin(), out.end(), out.begin(), ::toupper);
     return out;
   }
-  static std::string escape(const std::string &in) {
+  static std::string _escape(const std::string &in) {
     std::string out(in);
     if (in.find(' ') != std::string::npos)
       out = std::string("\"").append(out).append("\"");
     return out;
   }
-  static std::vector<std::string> split(const std::string &input,
-                                        const std::string &regex) {
+  static std::vector<std::string> _split(const std::string &input,
+                                         const std::string &regex) {
     // passing -1 as the submatch index parameter performs splitting
     std::regex re(regex);
     std::sregex_token_iterator first{input.begin(), input.end(), re, -1}, last;
     return {first, last};
   }
-  static bool not_space(int ch) { return !std::isspace(ch); }
-  static inline void ltrim(std::string &s, bool (*f)(int) = not_space) {
+  static bool _not_space(int ch) { return !std::isspace(ch); }
+  static inline void _ltrim(std::string &s, bool (*f)(int) = _not_space) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), f));
   }
-  static inline void rtrim(std::string &s, bool (*f)(int) = not_space) {
+  static inline void _rtrim(std::string &s, bool (*f)(int) = _not_space) {
     s.erase(std::find_if(s.rbegin(), s.rend(), f).base(), s.end());
   }
-  static inline void trim(std::string &s, bool (*f)(int) = not_space) {
-    ltrim(s, f);
-    rtrim(s, f);
+  static inline void _trim(std::string &s, bool (*f)(int) = _not_space) {
+    _ltrim(s, f);
+    _rtrim(s, f);
   }
-  static inline std::string ltrim_copy(std::string s,
-                                       bool (*f)(int) = not_space) {
-    ltrim(s, f);
+  static inline std::string _ltrim_copy(std::string s,
+                                        bool (*f)(int) = _not_space) {
+    _ltrim(s, f);
     return s;
   }
-  static inline std::string rtrim_copy(std::string s,
-                                       bool (*f)(int) = not_space) {
-    rtrim(s, f);
+  static inline std::string _rtrim_copy(std::string s,
+                                        bool (*f)(int) = _not_space) {
+    _rtrim(s, f);
     return s;
   }
-  static inline std::string trim_copy(std::string s,
-                                      bool (*f)(int) = not_space) {
-    trim(s, f);
+  static inline std::string _trim_copy(std::string s,
+                                       bool (*f)(int) = _not_space) {
+    _trim(s, f);
     return s;
   }
 
@@ -193,7 +193,7 @@ class ArgumentParser {
 };
 template <>
 inline std::string ArgumentParser::get<std::string>(const std::string &name) {
-  auto v = _variables.find(delimit(name));
+  auto v = _variables.find(_delimit(name));
   if (v != _variables.end()) return v->second;
   return "";
 }
@@ -205,7 +205,7 @@ template <>
 inline std::vector<std::string> ArgumentParser::getv<std::string>(
     const std::string &name) {
   std::string argstr = get<std::string>(name);
-  return split(argstr, "\\s");
+  return _split(argstr, "\\s");
 }
 
 #endif
