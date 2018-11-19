3c964c4c6499f0f4ccde040ad7c756789381514d
Jesse Laning <jlaning@ksu.edu>
2018-11-18 22:54:46 -0600

    license comment

--- argparse.h
+++ argparse.h
@@ -1,3 +1,19 @@
+/**
+ * argparse.h is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
+ *
+ * argparse.h is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with argparse.h.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Author: Jesse Laning
+ */
+
 #ifndef ARGPARSE_H
 #define ARGPARSE_H
 
--- example.cpp
+++ example.cpp
@@ -1,3 +1,19 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Author: Jesse Laning
+ */
+
 #include <iostream>
 #include "argparse.h"
 
