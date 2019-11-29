8dd451b99acfa34f9f127365c7c213a09f60984f
Jesse <thejesselaning@gmail.com>
2019-11-29 17:20:37 -0600

    add values test

--- tests.cpp
+++ tests.cpp
@@ -27,186 +27,192 @@ struct result {
     return {true, 0, "", "", ""};              \
   }
 
-TEST(
-    no_args, {
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-    }, )
+TEST(no_args, {
+  try {
+    parser.parse(argc, argv);
+  } catch (const ArgumentParser::ArgumentNotFound& ex) {
+    TASSERT(false, ex.what())
+  }
+}, )
 
-TEST(
-    short_optional_flag_exists,
-    {
-      parser.add_argument("-f", "a flag", false);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.exists("f"), "flag not found")
-    },
-    "-f")
+TEST(short_optional_flag_exists,
+     {
+       parser.add_argument("-f", "a flag", false);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("f"), "flag not found")
+     },
+     "-f")
 
-TEST(
-    short_optional_flag_does_not_exist, {
-      parser.add_argument("-f", "a flag", false);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(!parser.exists("f"), "flag found")
-    }, )
+TEST(short_optional_flag_does_not_exist, {
+  parser.add_argument("-f", "a flag", false);
+  try {
+    parser.parse(argc, argv);
+  } catch (const ArgumentParser::ArgumentNotFound& ex) {
+    TASSERT(false, ex.what())
+  }
+  TASSERT(!parser.exists("f"), "flag found")
+}, )
 
-TEST(
-    short_required_flag_exists,
-    {
-      parser.add_argument("-f", "a flag", true);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.exists("f"), "required flag not found")
-    },
-    "-f")
+TEST(short_required_flag_exists,
+     {
+       parser.add_argument("-f", "a flag", true);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("f"), "required flag not found")
+     },
+     "-f")
 
-TEST(
-    short_required_flag_does_not_exist, {
-      parser.add_argument("-f", "a flag", true);
-      bool failed = false;
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound&) {
-        failed = true;
-      }
-      TASSERT(failed, "required flag found")
-    }, )
+TEST(short_required_flag_does_not_exist, {
+  parser.add_argument("-f", "a flag", true);
+  bool failed = false;
+  try {
+    parser.parse(argc, argv);
+  } catch (const ArgumentParser::ArgumentNotFound&) {
+    failed = true;
+  }
+  TASSERT(failed, "required flag found")
+}, )
 
-TEST(
-    long_optional_flag_exists,
-    {
-      parser.add_argument("--flag", "a flag", false);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.exists("flag"), "flag not found")
-    },
-    "--flag")
+TEST(long_optional_flag_exists,
+     {
+       parser.add_argument("--flag", "a flag", false);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("flag"), "flag not found")
+     },
+     "--flag")
 
-TEST(
-    long_short_optional_flag_pair_exists,
-    {
-      parser.add_argument("-f", "--flag", "a flag", false);
-      parser.add_argument("-t", "--test", "a flag", false);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.exists("flag"), "flag not found")
-      TASSERT(parser.exists("f"), "flag not found")
-      TASSERT(parser.exists("test"), "flag not found")
-      TASSERT(parser.exists("t"), "flag not found")
-    },
-    "--flag", "-t")
+TEST(long_short_optional_flag_pair_exists,
+     {
+       parser.add_argument("-f", "--flag", "a flag", false);
+       parser.add_argument("-t", "--test", "a flag", false);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("flag"), "flag not found")
+       TASSERT(parser.exists("f"), "flag not found")
+       TASSERT(parser.exists("test"), "flag not found")
+       TASSERT(parser.exists("t"), "flag not found")
+     },
+     "--flag", "-t")
 
-TEST(
-    short_combined_flags,
-    {
-      parser.add_argument("-f", "a flag", false);
-      parser.add_argument("-g", "a flag", false);
+TEST(short_combined_flags,
+     {
+       parser.add_argument("-f", "a flag", false);
+       parser.add_argument("-g", "a flag", false);
 
-      parser.add_argument("-i", "a flag", false);
-      parser.add_argument("-j", "a flag", false);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.exists("f"), "f flag not found")
-      TASSERT(parser.exists("g"), "g flag not found")
-      TASSERT(parser.exists("i"), "f flag not found")
-      TASSERT(parser.exists("j"), "g flag not found")
-    },
-    "-fg", "-ji")
+       parser.add_argument("-i", "a flag", false);
+       parser.add_argument("-j", "a flag", false);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("f"), "f flag not found")
+       TASSERT(parser.exists("g"), "g flag not found")
+       TASSERT(parser.exists("i"), "f flag not found")
+       TASSERT(parser.exists("j"), "g flag not found")
+     },
+     "-fg", "-ji")
 
-TEST(
-    vector_flag_empty,
-    {
-      parser.add_argument("-v", "a flag", false);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.exists("v"), "flag not found")
-      auto v = parser.getv<int>("v");
-      TASSERT(v.size() == 0, "wrong vector values")
-    },
-    "-v")
+TEST(vector_flag_empty,
+     {
+       parser.add_argument("-v", "a flag", false);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("v"), "flag not found")
+       auto v = parser.getv<int>("v");
+       TASSERT(v.size() == 0, "wrong vector values")
+     },
+     "-v")
 
-TEST(
-    vector_flag,
-    {
-      parser.add_argument("-v", "a flag", false);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.exists("v"), "flag not found")
-      auto v = parser.getv<int>("v");
-      TASSERT(v.size() == 5, "wrong vector values")
-    },
-    "-v", "1", "2", "3", "4", "5")
+TEST(vector_flag,
+     {
+       parser.add_argument("-v", "a flag", false);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("v"), "flag not found")
+       auto v = parser.getv<int>("v");
+       TASSERT(v.size() == 5, "wrong vector values")
+     },
+     "-v", "1", "2", "3", "4", "5")
 
-TEST(
-    short_and_vector_flag,
-    {
-      parser.add_argument("-v", "a flag", false);
-      parser.add_argument("-b", "a flag", false);
-      parser.add_argument("-f", "a flag", false);
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.exists("b"), "flag not found")
-      TASSERT(parser.exists("v"), "flag not found")
-      TASSERT(parser.exists("f"), "flag not found")
-      auto v = parser.getv<int>("v");
-      TASSERT(v.size() == 5, "wrong vector values")
-    },
-    "-b", "0", "-v", "1", "2", "3", "4", "5", "-f", "6", "7", "8")
+TEST(short_and_vector_flag,
+     {
+       parser.add_argument("-v", "a flag", false);
+       parser.add_argument("-b", "a flag", false);
+       parser.add_argument("-f", "a flag", false);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("b"), "flag not found")
+       TASSERT(parser.exists("v"), "flag not found")
+       TASSERT(parser.exists("f"), "flag not found")
+       auto v = parser.getv<int>("v");
+       TASSERT(v.size() == 5, "wrong vector values")
+     },
+     "-b", "0", "-v", "1", "2", "3", "4", "5", "-f", "6", "7", "8")
 
-TEST(
-    short_help_flag,
-    {
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.is_help(), "help not found")
-    },
-    "-h")
+TEST(short_help_flag,
+     {
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.is_help(), "help not found")
+     },
+     "-h")
 
-TEST(
-    long_help_flag,
-    {
-      try {
-        parser.parse(argc, argv);
-      } catch (const ArgumentParser::ArgumentNotFound& ex) {
-        TASSERT(false, ex.what())
-      }
-      TASSERT(parser.is_help(), "help not found")
-    },
-    "--help")
+TEST(long_help_flag,
+     {
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.is_help(), "help not found")
+     },
+     "--help")
+
+TEST(flag_values,
+     {
+       parser.add_argument("-f", "--flag", "a flag", false);
+       parser.add_argument("-t", "--test", "a flag", false);
+       parser.add_argument("-g", "a flag", false);
+       try {
+         parser.parse(argc, argv);
+       } catch (const ArgumentParser::ArgumentNotFound& ex) {
+         TASSERT(false, ex.what())
+       }
+       TASSERT(parser.exists("flag"), "flag not found")
+       TASSERT(parser.get<int>("flag") == 2, "wrong flag value")
+       TASSERT(parser.exists("t"), "flag not found")
+       TASSERT(parser.get<int>("t") == 3, "wrong flag value")
+       TASSERT(parser.exists("g"), "flag not found")
+       TASSERT(parser.get<double>("g") == 0.125, "wrong flag value")
+     },
+     "-f", "2", "--test", "3", "-g", "0.125")
 
 #define TT(name) \
   { #name, name }
@@ -223,7 +229,8 @@ std::map<std::string, test> tests{TT(no_args),
                                   TT(vector_flag),
                                   TT(short_and_vector_flag),
                                   TT(short_help_flag),
-                                  TT(long_help_flag)};
+                                  TT(long_help_flag),
+                                  TT(flag_values)};
 
 int main(int argc, const char* argv[]) {
   std::vector<result> results;
